00efc13eca059114e81ae211eb58950de98841e7
Muhammet Kaya <101904041+kaya2DLL@users.noreply.github.com>
2023-04-28 16:03:44 +0300

    connection model

---
+++ src/app/model.ts
@@ -0,0 +1,29 @@
+export class Model{
+    user;
+    item;
+
+    /**
+     *
+     */
+    constructor() {
+        this.user="kaya"
+        this.item=[
+            new TodoItem("Spor",true),
+            new TodoItem("Kahvaltı",false),
+            new TodoItem("Kitap Okumak",false),
+            new TodoItem("Sinema",false)
+         
+        ];
+        
+    }
+}
+export class TodoItem{
+     description;
+    action;
+
+   
+    constructor(description: string,action: boolean) {
+        this.description=description;
+        this.action=action;
+    }
+}
